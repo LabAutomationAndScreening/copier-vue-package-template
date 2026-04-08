707a15c8a4ccd8792ad881fa16dfee78a38990dc
zender <github@nathanzender.com>
2026-04-08 18:55:08 +0000

    Formatting

--- template/vitest.config.ts
+++ template/vitest.config.ts
@@ -4,12 +4,12 @@ import { fileURLToPath } from "node:url";
 import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
-    resolve: {
-        alias: {
-            "~": fileURLToPath(new URL("./src/runtime", import.meta.url)),
-        },
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./src/runtime", import.meta.url)),
     },
-    test: {
-        include: ["test/**/*.spec.ts", "test/**/*.test.ts"],
-    },
+  },
+  test: {
+    include: ["test/**/*.spec.ts", "test/**/*.test.ts"],
+  },
 });
