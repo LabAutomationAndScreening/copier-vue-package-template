1c2e9f1949299a6ea42691c021b35cf2f0666692
zender <github@nathanzender.com>
2026-05-13 13:48:04 +0000

    Test

--- template/test/basic.spec.ts
+++ template/test/basic.spec.ts
@@ -1,10 +1,10 @@
-import { fileURLToPath } from "node:url";
+import { resolve } from "node:path";
 import { $fetch, setup } from "@nuxt/test-utils/e2e";
 import { describe, expect, it } from "vitest";
 
 describe("verify basic fixture test (useful as an example, update for your app)", async () => {
   await setup({
-    rootDir: fileURLToPath(new URL("./fixtures/basic", import.meta.url)),
+    rootDir: resolve(__dirname, "fixtures/basic"),
   });
 
   it("renders the index page", async () => {
